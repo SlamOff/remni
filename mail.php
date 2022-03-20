a54a2ba53a91198b69f8ef3040d75eac2555e26d
Vitalii Pustovyi <vpustovyy@atlassian.com>
2022-03-20 16:55:20 +0200

    20

--- mail.php
+++ mail.php
@@ -16,7 +16,8 @@
     try {
         foreach ( $_POST as $key => $value ) {
             if ( $value != "" ) {
-                $noHTMLmessage .= "$key: $value".PHP_EOL;
+                $noHTMLmessage .= nl2br("$key: $value".PHP_EOL);
+                
                 $message .= "
                 " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
                     <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>

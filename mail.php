987bf85e78b980540e9ffbac5e037ad1829d199c
Vitalii Pustovyi <vpustovyy@atlassian.com>
2022-03-20 16:30:32 +0200

    20

--- mail.php
+++ mail.php
@@ -4,18 +4,24 @@
     use PHPMailer\PHPMailer\Exception;
     require 'vendor/autoload.php';
 
+    function adopt($text) {
+        return '=?UTF-8?B?'.base64_encode($text).'?=';
+    }
+    $c = true;
     $mail = new PHPMailer(true);
+    $subject = 'Заявка с лендинга';
+    $message = "<table style='width: 100%;'>$message</table>";
 
     try {
         foreach ( $_POST as $key => $value ) {
-            if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+            if ( $value != "" ) {
                 $message .= "
                 " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
                 <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
                 <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
             </tr>
             ";
-         }
+            }
         }
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;
@@ -28,7 +34,7 @@
         $mail->Port       = 465;
 
         //Recipients
-        $mail->setFrom('kremen.lendos@gmail.com', 'Заявка с лендинга');
+        $mail->setFrom($subject);
         $mail->addAddress('kremen.lendos@gmail.com', 'Serhii');
         //$mail->addAddress('vetalsd2@gmail.com');               //Name is optional
         // $mail->addReplyTo('info@example.com', 'Information');
@@ -41,8 +47,8 @@
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Заявка с лендинга';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->Subject = adopt($subject);
+        $mail->Body    = $message;
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();

ee819afbb66497eaebf57a52a9057f7968a33e6a
Vitalii Pustovyi <vpustovyy@atlassian.com>
2022-03-22 19:12:00 +0200

    referer

--- mail.php
+++ mail.php
@@ -20,7 +20,7 @@
                 
                 $message .= "
                 " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-                    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+                    <td style='padding: 10px; border: #e9e9e9 1px solid;width:150px;'><b>$key</b></td>
                     <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
                 </tr>
                 ";

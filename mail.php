e3fa87bbbb590793f53a5e5317a061df8b1f0153
Vitalii Pustovyi <vpustovyy@atlassian.com>
2022-03-22 18:54:35 +0200

    referer

--- mail.php
+++ mail.php
@@ -34,12 +34,12 @@
         foreach ( $utms as $key ) {
             
             if ( $key != "" ) {
-                foreach ( explode("=", $key) as $keey ) {
+                foreach ( explode("=", $key) as $keyInner ) {
                     var_dump($keey);
                     $message .= "
                     " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-                        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>". $key[0] ."</b></td>
-                        <td style='padding: 10px; border: #e9e9e9 1px solid;'>".$key[1]."</td>
+                        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>".$keyInner[0]."</b></td>
+                        <td style='padding: 10px; border: #e9e9e9 1px solid;'>".$keyInner[1]."</td>
                     </tr>
                     ";
                 }

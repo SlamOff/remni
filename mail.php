52501f0779328957ed9e26a4a706076b52ea7209
Vitalii Pustovyi <vpustovyy@atlassian.com>
2022-03-22 18:22:54 +0200

    referer

--- mail.php
+++ mail.php
@@ -32,7 +32,10 @@
         //var_dump($utms);
 
         foreach ( $utms as $key ) {
-            var_dump($key);
+            
+            if ( $key != "" ) {
+                var_dump($key);
+            }
             // $message .= "
             // " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
             //     <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>

e4ad85a0a4a0741b606ef1b94f245382092dd87b
Vitalii Pustovyi <vpustovyy@atlassian.com>
2022-03-22 18:08:10 +0200

    referer

--- mail.php
+++ mail.php
@@ -29,10 +29,10 @@
         }
         $message .= '<b>Заявка пришла со страницы:</b> ' . $_SERVER["HTTP_REFERER"] .'<br>';
         // "utm_source=kon_fb01.22&utm_medium=ll2%_int_opt&utm_campaign=din_kreativ"
-        $utms = explode("&", parse_url($url, PHP_URL_QUERY));
+        //$utms = explode("&", parse_url($url, PHP_URL_QUERY));
         // var_dump($utms[0]);
 
-        foreach ( $utms as $key => $value ) {
+        foreach ( explode("&", parse_url($url, PHP_URL_QUERY)) as $key => $value ) {
                 
             $message .= "
             " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
